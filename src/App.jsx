b0349f2863fc27aa9bb284c25c3878659fcf4d4c
REACT <mechainadam@gmail>
2024-03-20 15:57:06 +0100

    Dark Mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,40 @@ import Card from './components/Card';
 import { cardIcon } from './assets/icons.js';
 import data from './components/data.js';
 
+import { useState } from 'react';
+
 function App() {
 
   const allCardsList = data.map((cardData, index) => (
-    <li key={index}>
+    <li className='flex w-full' key={index}>
       <Card data={cardData} icons={cardIcon} />
     </li>
   ));
 
+  const [LightMode, setLightkMode] = useState(true);
+  const handlerClick = () => {
+    setLightkMode(!LightMode);
+  }
+  const classMode = LightMode ? "" : "light";
+  const classSwitch = LightMode ? "" : "flex-row-reverse";
+
   return (
-  <> 
-  <body class="flex flex-col gap-8 justify-center  items-center mx-6">
-     <ul>{allCardsList}</ul>
-  </body> 
-  </>
+  <div className={`${classMode} bg-background`}> 
+  <section >
+    <h1 className='text-foreground font-medium text-3xl'>Social Media Dashboard</h1>
+    <p className='text-cardforeground text-xl font-medium'>Total followers: </p>
+  </section>
+  
+
+  <section className='flex justify-between'>
+    <p className='text-xl font-medium text-cardforeground'>{LightMode ? "Light" : "Dark"} Mode</p> <button className={`inline-flex items-center w-16 h-9 rounded-full border-4 border-foreground before:block before:w-6 before:h-6 before:bg-foreground before:rounded-full before:mx-1 ${classSwitch}`} onClick={handlerClick}><span className='sr-only'>Change</span></button>
+  </section>
+
+  <article className="">
+     <ul className='flex flex-col gap-6 justify-center items-center'>{allCardsList}</ul>
+  </article>
+  
+  </div>
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,18 @@
 function Card({data, icons}){
   const iconUpDown = data.nb > 0 ? icons.up : icons.down;
-  const textClass = data.nb > 0 ? "text-green-500" : "text-red-500";
+  const textClass = data.nb > 0 ? "text-up" : "text-down";
+  const textNb = data.nb > 1000 ? `${Math.floor(data.nb / 1000)}k` : data.nb;
+  const Nb1k = data.followers > 10000 ? `${Math.floor(data.followers / 1000)}k` : data.followers;
 
     return (
       <>
         <section className={`bg-cardbackground w-full h-fit flex justify-center items-center rounded-lg
-        before:block before:absolute before:-inset-1 before:h-3 before:bg-${data.platform} relative overflow-hidden`}>
+        before:block before:absolute before:-inset-1 before:h-2 before:bg-${data.platform} relative overflow-hidden`}>
             <ul className="flex flex-col gap-2 py-8">
-                <li className="flex gap-2 justify-center"><img src={icons[data.platform]} alt={data.platform} /> <p className="font-medium text-cardforeground text-sm">{data.name}</p></li>
-                <li className="flex justify-center"><p className="font-bold text-6xl text-foreground mt-6">{data.followers}</p></li>
+                <li className="flex gap-2 justify-center"><img src={icons[data.platform]} alt={data.platform} /> <p className="font-medium text-cardforeground text-base">{data.name}</p></li>
+                <li className="flex justify-center"><p className="font-bold text-6xl text-foreground mt-6">{Nb1k}</p></li>
                 <li className="flex justify-center"><p className="uppercase tracking-[.25rem] text-cardforeground text-sm mb-6">Followers</p></li>
-                <li className="flex justify-center items-center gap-1"><img src={iconUpDown} alt="Arrow" /><p className={`font-medium text-sm ${textClass}`}>{data.nb} Today</p></li>
+                <li className="flex justify-center items-center gap-1"><img src={iconUpDown} alt="Arrow" /><p className={`font-medium text-sm ${textClass}`}>{textNb} Today</p></li>
             </ul>
         </section>
       </>
